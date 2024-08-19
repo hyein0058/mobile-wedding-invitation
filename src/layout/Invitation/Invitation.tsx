3d09c444699a66c696b904e3b0b80eabcd936928
yonghyun.lee <yonghyun.lee@coway.com>
2024-08-19 15:42:25 +0900

    feat: 구글 캘린더 추가하기 구현

--- src/layout/Invitation/Invitation.tsx
+++ src/layout/Invitation/Invitation.tsx
@@ -1,20 +1,38 @@
 import styled from '@emotion/styled';
 import data from 'data.json';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 import Host from '../Contact/Host.tsx';
 import RoundButton from '@/components/RoundButton.tsx';
 import { Caption, Paragraph } from '@/components/Text.tsx';
+import Calendar from '@/layout/WeddingDate/Calendar.tsx';
+import WeddingDday from '@/layout/WeddingDate/WeddingDday.tsx';
+
+dayjs.extend(utc);
 
 const Invitation = () => {
   const { greeting } = data;
+
+  const eventDetails = {
+    title: '전홍준 \uD83D\uDC8D 최혜인 결혼식',
+    startDateTime: dayjs('2024-11-24 17:00').utc().format('YYYYMMDDTHHmmss') + 'Z',
+    endDateTime: dayjs('2024-11-24 18:00').utc().format('YYYYMMDDTHHmmss') + 'Z',
+    details: greeting.message,
+    location: 'Seoul, South Korea',
+  };
+
+  const googleCalendarUrl = `https://calendar.google.com/calendar/r/eventedit?text=${encodeURIComponent(eventDetails.title)}&dates=${eventDetails.startDateTime}/${eventDetails.endDateTime}&details=${encodeURIComponent(eventDetails.details)}&location=${encodeURIComponent(eventDetails.location)}&sf=true&output=xml`;
+
   return (
     <InvitationWrapper>
       <Paragraph>{greeting.message}</Paragraph>
       <Host />
       <Caption textAlign={'center'}>{greeting.eventDetail}</Caption>
-      {/* TODO: 구글캘린더 추가하기 기능을 넣는다면 링크 수정 */}
+      <Calendar />
+      <WeddingDday />
       <RoundButton
         target="_blank"
-        href=""
+        href={googleCalendarUrl}
         rel="noreferrer">
         구글 캘린더 추가하기
       </RoundButton>
