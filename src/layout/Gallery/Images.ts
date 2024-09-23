7ebefe4e684c3d492005d132df92cc0080170ca5
chi <chi@coway.com>
2024-09-23 11:02:22 +0900

    feat: mobile 분기 추가

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -7,69 +7,76 @@ import image01 from '@/assets/images/WUNE0753-1.jpg';
 import image02 from '@/assets/images/WUNE0815-1.jpg';
 import image07 from '@/assets/images/WUNE1090-1.jpg';
 
+const {userAgent} = navigator
+const isMobile = userAgent.match(/(iPhone|Android)/)
+
+const height = isMobile ? '130px' : '150px'
+const wideWidth = isMobile ? '160px' : '200px'
+const width = isMobile ? '70px' : '100px'
+
 const images = [
   /* 이미지 경로를 리스트로 저장 */
   {
     alt: 'image01',
     source: image01,
-    width: '70px',
-    height: '130px',
+    width: width,
+    height: height,
     previewWidth: '1280',
     previewHeight: '1920',
   },
   {
     alt: 'image02',
     source: image02,
-    width: '160px',
-    height: '130px',
+    width: wideWidth,
+    height: height,
     previewWidth: '1920',
     previewHeight: '1280',
   },
   {
     alt: 'image03',
     source: image03,
-    width: '160px',
-    height: '130px',
+    width: wideWidth,
+    height: height,
     previewWidth: '1920',
     previewHeight: '1280',
   },
   {
     alt: 'image04',
     source: image04,
-    width: '70px',
-    height: '130px',
+    width: width,
+    height: height,
     previewWidth: '1280',
     previewHeight: '1920',
   },
   {
     alt: 'image05',
     source: image05,
-    width: '70px',
-    height: '130px',
+    width: width,
+    height: height,
     previewWidth: '1280',
     previewHeight: '1920',
   },
   {
     alt: 'image06',
     source: image06,
-    width: '160px',
-    height: '130px',
+    width: wideWidth,
+    height: height,
     previewWidth: '1920',
     previewHeight: '1280',
   },
   {
     alt: 'image07',
     source: image07,
-    width: '160px',
-    height: '130px',
+    width: wideWidth,
+    height: height,
     previewWidth: '1920',
     previewHeight: '1280',
   },
   {
     alt: 'image08',
     source: image08,
-    width: '70px',
-    height: '130px',
+    width: width,
+    height: height,
     previewWidth: '1280',
     previewHeight: '1920',
   }
